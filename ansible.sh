245299abc9f90388c16a7fca451a1a1f9cfe7133
Navya Bhogadhi <navya.bgd@outlook.com>
2025-04-15 12:30:24 +0530

    Update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -1,2 +1,7 @@
-amazon-linux-extras install ansible2 -y
+amazon-linux-extras install ansible2 -y - linux 2
+sudo dnf install -y ansible - linux 2023
+
+
 yum install python3 python-pip python-dlevel -y
+
+sudo dnf install -y python3 python3-pip
